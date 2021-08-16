f36e5a7bce56f9e76c31cb436d4f0afb5ec5173a
raghadtt <60436784+raghadtt@users.noreply.github.com>
2021-08-17 01:22:34 +0300

    Final update

--- frontend.py
+++ frontend.py
@@ -10,17 +10,22 @@ app = Flask(__name__)
 cache.init_app(app)
 
 api = Api(app)
-
-countCatalog,countOrder = 0
+count = 0
+arr = [None] * 50
+i=0
+count2=0
+countcatalog = 1
+countorder = 1
 
 class Search(Resource):
 
     @cache.memoize(50)
-    def get(self, name, countcatalog = countCatalog):
+    def get(self, name):
 
         global count
         global arr
         global i
+        global countcatalog
 
         data = cache.get(name)
         if data:
@@ -29,12 +34,12 @@ class Search(Resource):
             if(countcatalog):
                 #catalog server A
                 countcatalog = 0
-                resp =requests.get('http://192.168.1.101:5000/search/'+str(name)).json() #Catalog server1 IP
+                resp =requests.get('http://192.168.1.110:5000/search/'+str(name)).json() #Catalog serverA IP
                 cache.set(name, resp)
             else:
                 #catalog server B
                 countcatalog = 1
-                resp =requests.get('http://192.168.1.101:5000/search/'+str(name)).json() #Catalog server0 IP
+                resp =requests.get('http://192.168.1.109:5000/search/'+str(name)).json() #Catalog serverB IP
                 cache.set(name, resp)
         arr[count] = name
         count = count + 1
@@ -43,16 +48,20 @@ class Search(Resource):
             cache.delete(arr[i])
             i= i+1
 
-        return {'items':resp}
+        if(countcatalog):
+           return {'items from B': resp}
+        else:  
+           return {'items from A': resp}
 
 
 class Info(Resource):
 
     @cache.memoize(50)
-    def get(self, num, countcatalog = countCatalog):
+    def get(self, num):
         global count
         global arr
         global i
+        global countcatalog
 
         data = cache.get(str(num))
         if data:
@@ -61,12 +70,12 @@ class Info(Resource):
             if(countcatalog):
                 #catalog server A
                 countcatalog = 0
-                resp =requests.get('http://192.168.1.101:5000/info/'+str(num)).json() #Catalog server1 IP
+                resp =requests.get('http://192.168.1.110:5000/info/'+str(num)).json() #Catalog server1 IP
                 cache.set(str(num), resp)
             else:
                 #catalog server B
                 countcatalog = 1
-                resp =requests.get('http://192.168.1.101:5000/info/'+str(num)).json() #Catalog server0 IP
+                resp =requests.get('http://192.168.1.109:5000/info/'+str(num)).json() #Catalog server0 IP
                 cache.set(str(num), resp)
 
         arr[count] = str(num)
@@ -75,21 +84,26 @@ class Info(Resource):
         if count > 6:
             cache.delete(arr[i])
             i= i+1
-
-        return {'items': resp}
+        if(countcatalog):
+           return {'items from B': resp}
+        else:  
+           return {'items from A': resp}
 
 class Purchase(Resource):
 
-    def put(self, num, countorder=countOrder):
+    def put(self, num):
         #from order server
+        
+        global countorder
+        
         if(countorder):
             #catalog server A
             countorder = 0
-            return requests.put('http://192.168.1.108:5000/purchase/'+str(num)).json() #Order server0 IP
+            return requests.put('http://192.168.1.110:5001/purchase/'+str(num)).json() #Order server0 IP
         else:
             #catalog server B
             countorder = 1
-            return requests.put('http://192.168.1.108:5000/purchase/'+str(num)).json() #Order server1 IP
+            return requests.put('http://192.168.1.109:5001/purchase/'+str(num)).json() #Order server1 IP
 
 
 #implement cache consistency after any update   
